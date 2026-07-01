49e0c0a85fe5e2a1093bf401d1b563e615534543
grkmr <goerkem@posteo.com>
2026-07-01 12:39:31 +0200

    :arrow_up: Bump plugin version

--- src/totem/plugin.py
+++ src/totem/plugin.py
@@ -29,7 +29,7 @@ class Totem(Plugin):
         "Generate Temporal Object Type Models (TOTeM) to uncover type-level temporal and cardinality relations"
         " in event logs"
     )
-    version = "1.7.2"
+    version = "1.7.3"
 
     @plugin_method(label="Discover TOTeM", description="Discovers a Temporal Object Type Model")
     def mine_totem(self, ocel: Annotated[OCEL, OCELAnnotation(label="Event Log")], input: MineInput) -> TotemGraph:
